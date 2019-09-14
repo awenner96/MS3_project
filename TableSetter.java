44bf3006379da656b1ecafda0fe95085f87d08a5
awenner96 <55173488+awenner96@users.noreply.github.com>
2019-09-14 00:16:36 -0400

    Update TableSetter.java

--- TableSetter.java
+++ TableSetter.java
@@ -3,6 +3,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+//I have never before interacted with creating an SQLite table using Java, so I found a tutorial and wrote this based off of it.
+//After creating the SQLiteConnect class I wasn't sure it was necessary because it looks like one just uses this a simple try connection
+//statement here to connect to the database.
+//However, even with only labeling one primary key field, I couldn't get anything to show up.
+
 public class TableSetter {
     public static void tablecreator(){
         String url = "jdbc:sqlite:C://";
