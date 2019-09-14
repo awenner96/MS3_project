4726fce595d6bc87311d46a4feb94a84300511ca
awenner96 <55173488+awenner96@users.noreply.github.com>
2019-09-14 00:00:08 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,12 @@
 public class Main {
-    //the CSV file to be parsed.
-    public String CSVfile;
-
+    //Main method that pulls all other classes together
     public static void main (String[] args){
+        String CSVfile = "C:Users\\User\\Downloads\\ms3Interview.csv";
         //first connect to the database
         SQLiteConnect.main(null);
         //Second parse the given csv file
-        Parser parserconstructor = new Parser();
-        //Third make the table
+        Parser parserconstructor = new Parser(CSVfile);
+        //Third build the table
         TableSetter.main(null);
-        
-
-
     }
 }
