f358f2224b8381a7dde797d12da6d0aeed54aab4
awenner96 <55173488+awenner96@users.noreply.github.com>
2019-09-14 00:19:12 -0400

    Update SQLiteConnect.java

--- SQLiteConnect.java
+++ SQLiteConnect.java
@@ -2,6 +2,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+//This along with the tablecreator I had to view tutorials in order to come up with the code.
+//I wasn't sure how to initialize the database even after downloading the appropriate drivers onto my desktop computer.
+//File structure, code, or both was wrong and I never managed to get anything running.
+
 public class SQLiteConnect {
     public static void main(String[] args){
         Connection connection = null;
